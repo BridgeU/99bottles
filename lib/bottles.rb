b0d7efc789e51a24b5839d7132dff878e8c13d73
Sam Jewell <sam@bridge-u.com>
2017-03-15 13:41:09 +0000

    No more bottles (including capitalisation) and trip to store

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,12 +7,24 @@ class Bottles
 
 
     <<~HEREDOC
-      #{num_bottles(num)} of beer on the wall, #{num_bottles(num)} of beer.
-      Take #{article(num)} down and pass it around, #{num_bottles(num-1)} of beer on the wall.
+      #{first_line(num)}
+      #{second_line(num)}
     HEREDOC
   end
 
   private
+
+  def first_line(num)
+    "#{num_bottles(num)} of beer on the wall, #{num_bottles(num)} of beer.".capitalize
+  end
+
+  def second_line(num)
+    if num == 0
+      "Go to the store and buy some more, 99 bottles of beer on the wall."
+    else
+      "Take #{article(num)} down and pass it around, #{num_bottles(num-1)} of beer on the wall."
+    end
+  end
 
   def article(num)
     num == 1? "it" : "one"
