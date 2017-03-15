07c62f14e3edacfd568996674ade7139e70a05b4
Sam Jewell <sam@bridge-u.com>
2017-03-15 13:24:40 +0000

    Pass the first test - class and verse added

---
+++ lib/bottles.rb
@@ -0,0 +1,10 @@
+class Bottles
+
+  def verse(num)
+    <<~HEREDOC
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    HEREDOC
+  end
+
+end
