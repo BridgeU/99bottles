15998f3c1d94981545cd51e8177cea119a69e3d5
Sam Jewell <sam@bridge-u.com>
2017-03-15 13:31:30 +0000

    Pass the third test - singularise bottles into bottle

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,10 +4,18 @@ class Bottles
 
   def verse(num)
     # binding.pry
+
+
     <<~HEREDOC
-      #{num} bottles of beer on the wall, #{num} bottles of beer.
-      Take one down and pass it around, #{num-1} bottles of beer on the wall.
+      #{num_bottles(num)} of beer on the wall, #{num_bottles(num)} of beer.
+      Take one down and pass it around, #{num_bottles(num-1)} of beer on the wall.
     HEREDOC
+  end
+
+  private
+
+  def num_bottles(num)
+    num == 1? "1 bottle" : "#{num} bottles"
   end
 
 end
