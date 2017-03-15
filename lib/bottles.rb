c4a05862355eb101fe36d7d83f2cc87a1124c425
Dave Millican <dave@bridge-u.com>
2017-03-15 13:30:58 +0000

    add pluralisation of bottle for numbers other than 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,20 @@
 class Bottles
   def verse(number)
     next_number = number - 1
-<<-VERSE
-#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{next_number} bottles of beer on the wall.
+
+    <<-VERSE
+#{number_of_bottles(number)} of beer on the wall, #{number_of_bottles(number)} of beer.
+Take one down and pass it around, #{number_of_bottles(next_number)} of beer on the wall.
 VERSE
   end
+
+  def number_of_bottles(number)
+    if number == 1
+      bottle_name = "bottle"
+    else
+      bottle_name = "bottles"
+    end
+    "#{number} #{bottle_name}"
+  end
+
 end
