5633a653bbc27e83cec467db5743524a656ac955
Sam Jewell <sam@bridge-u.com>
2017-03-15 13:28:46 +0000

    Pass the second test - use the argument to generate the string

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 class Bottles
 
   def verse(num)
+    # binding.pry
     <<~HEREDOC
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
+      #{num} bottles of beer on the wall, #{num} bottles of beer.
+      Take one down and pass it around, #{num-1} bottles of beer on the wall.
     HEREDOC
   end
 
