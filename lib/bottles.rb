031f3659487c081b8b4211d0287542a85ee422e4
doutatsu <doutatsu@me.com>
2017-03-22 12:21:08 +0000

    Pass third test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,19 @@
 class Bottles
   def verse(number)
+    plural_string = pluralize(number - 1, "bottle", "bottles")
     string = %(#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+Take one down and pass it around, #{plural_string} of beer on the wall.
 )
     string
+  end
+
+  def pluralize(n, singular, plural=nil)
+    if n == 1
+        "1 #{singular}"
+    elsif plural
+        "#{n} #{plural}"
+    else
+        "#{n} #{singular}s"
+    end
   end
 end
