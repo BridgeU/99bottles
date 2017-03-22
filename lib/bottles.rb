e22d190df8891ddf3cf9e6376c896110110d63d4
doutatsu <doutatsu@me.com>
2017-03-22 12:30:04 +0000

    Pass forth test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,19 +1,21 @@
 class Bottles
   def verse(number)
-    plural_string = pluralize(number - 1, "bottle", "bottles")
-    string = %(#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{plural_string} of beer on the wall.
+    plural_string = lambda{ |num| pluralize(num, 'bottle', 'bottles') }
+    string = %(#{plural_string.call(number)} of beer on the wall, #{plural_string.call(number)} of beer.
+Take #{number == 1 ? "it" : "one"} down and pass it around, #{plural_string.call(number - 1)} of beer on the wall.
 )
     string
   end
 
-  def pluralize(n, singular, plural=nil)
-    if n == 1
-        "1 #{singular}"
+  def pluralize(n, singular, plural = nil)
+    if n.zero?
+      'no more bottles'
+    elsif n == 1
+      "1 #{singular}"
     elsif plural
-        "#{n} #{plural}"
+      "#{n} #{plural}"
     else
-        "#{n} #{singular}s"
+      "#{n} #{singular}s"
     end
   end
 end
