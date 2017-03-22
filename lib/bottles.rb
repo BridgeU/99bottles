142d0c65c2bc0087aeb9db2de6b1e8f299e2aac2
doutatsu <doutatsu@me.com>
2017-03-22 12:52:21 +0000

    Pass 5th test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
   def verse(number)
     plural_string = lambda{ |num| pluralize(num, 'bottle', 'bottles') }
-    string = %(#{plural_string.call(number)} of beer on the wall, #{plural_string.call(number)} of beer.
-Take #{number == 1 ? "it" : "one"} down and pass it around, #{plural_string.call(number - 1)} of beer on the wall.
+    string = %(#{plural_string.call(number).capitalize} of beer on the wall, #{plural_string.call(number)} of beer.
+#{shop(number)}, #{plural_string.call(number - 1)} of beer on the wall.
 )
     string
   end
@@ -12,10 +12,20 @@ Take #{number == 1 ? "it" : "one"} down and pass it around, #{plural_string.call
       'no more bottles'
     elsif n == 1
       "1 #{singular}"
+    elsif n < 0
+      pluralize(100 + n, 'bottle', 'bottles')
     elsif plural
       "#{n} #{plural}"
     else
       "#{n} #{singular}s"
+    end
+  end
+
+  def shop(num)
+    if num > 0
+      "Take #{num == 1 ? "it" : "one"} down and pass it around"
+    else
+      "Go to the store and buy some more"
     end
   end
 end
