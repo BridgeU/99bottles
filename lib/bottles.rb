444e1d9c2c29da0c01abccb4e5f2f7b1e516083b
hannako <jessjones1@gmail.com>
2017-03-22 11:46:53 +0000

    second test green

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,8 +2,8 @@ class Bottles
 
   def verse(n)
     <<~HEREDOC
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
+      #{n} bottles of beer on the wall, #{n} bottles of beer.
+      Take one down and pass it around, #{n-1} bottles of beer on the wall.
     HEREDOC
   end
 
