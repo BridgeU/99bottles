a32227fff199300c65a63e4969f37e09d189d1f2
hannako <jessjones1@gmail.com>
2017-03-22 11:45:07 +0000

    first test green

---
+++ lib/bottles.rb
@@ -0,0 +1,11 @@
+class Bottles
+
+  def verse(n)
+    <<~HEREDOC
+      99 bottles of beer on the wall, 99 bottles of beer.
+      Take one down and pass it around, 98 bottles of beer on the wall.
+    HEREDOC
+  end
+
+
+end
