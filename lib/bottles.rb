d4e7f3d7c11bbb2bc0c2a3d15118a84cad23d76f
hannako <jessjones1@gmail.com>
2017-03-22 12:07:50 +0000

    fourth test green

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,12 +3,20 @@ class Bottles
   def verse(n)
     <<~HEREDOC
       #{bottles(n)} of beer on the wall, #{bottles(n)} of beer.
-      Take one down and pass it around, #{bottles(n-1)} of beer on the wall.
+      Take #{pronoun(n)} down and pass it around, #{bottles(n-1)} of beer on the wall.
     HEREDOC
   end
 
   def bottles(n)
-    n == 1 ? "#{n} bottle" : "#{n} bottles"
+    case n
+    when 1 then "#{n} bottle"
+    when 0 then "no more bottles"
+    else "#{n} bottles"
+    end
+  end
+
+  def pronoun(n)
+    n == 1? 'it' : 'one'
   end
 
 end
