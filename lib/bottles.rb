374059e2029376664aafa0605fcaa8255a9e9be3
Gonçalo Morais <gnclmorais@gmail.com>
2017-03-29 13:25:38 +0100

    Finished!

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,17 +1,59 @@
 class Bottles
   def verse(number)
-    "#{pluralize(number, 'bottle')} of beer on the wall, "\
-    "#{pluralize(number, 'bottle')} of beer."\
-    "\n"\
-    "Take one down and pass it around, "\
-    "#{next_bottles(number)} of beer on the wall."\
-    "\n"
+    @nr_bottles = number
+    sing
+  end
+
+  def verses(start, finish)
+    (finish..start).to_a.reverse.inject([]) do |song, bottles|
+      @nr_bottles = bottles
+      song << sing
+    end.join("\n")
+  end
+
+  def song
+    verses(99, 0)
   end
 
   private
 
+  def sing
+    "#{how_many_bottles.capitalize} of beer on the wall, "\
+    "#{how_many_bottles} of beer."\
+    "\n"\
+    "#{what_to_do}, "\
+    "#{how_many_bottles} of beer on the wall."\
+    "\n"
+  end
+
+  def what_to_do
+    if @nr_bottles > 0
+      message = "Take #{one_or_it} down and pass it around"
+      @nr_bottles -= 1
+    else
+      message = "Go to the store and buy some more"
+      @nr_bottles = 99
+    end
+
+    message
+  end
+
+  def how_many_bottles
+    message = if @nr_bottles > 0
+      pluralize(@nr_bottles, "bottle")
+    else
+      "no more bottles"
+    end
+  end
+
+  #--
+
+  def one_or_it
+    @nr_bottles > 1 ? "one" : "it"
+  end
+
   def next_bottles(number)
-    if number > 0
+    if number - 1 > 0
       pluralize(number - 1, "bottle")
     else
       "no more bottles"
