1a042e14732fcb0d94e6c3dfce420f0c0e85deb9
Gonçalo Morais <gnclmorais@gmail.com>
2017-03-29 12:22:38 +0100

    Pluralize

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,24 @@
 class Bottles
   def verse(number)
+    "#{pluralize(number, 'bottle')} of beer on the wall, "\
+    "#{pluralize(number, 'bottle')} of beer."\
+    "\n"\
+    "Take one down and pass it around, "\
+    "#{next_bottles(number)} of beer on the wall."\
+    "\n"
+  end
+
+  private
+
+  def next_bottles(number)
+    if number > 0
+      pluralize(number - 1, "bottle")
+    else
+      "no more bottles"
+    end
+  end
+
+  def pluralize(number, string)
+    "#{number} #{string}#{number > 1 ? 's' : ''}"
   end
 end
