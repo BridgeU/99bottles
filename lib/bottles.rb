5d13367e40f4ff9934d58c7a798076330d3f390d
Gonçalo Morais <gnclmorais@gmail.com>
2017-03-29 13:40:14 +0100

    Remove redundant method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,19 +1,5 @@
 class Bottles
-  def verse(number)
-    sing(number)
-  end
-
-  def verses(start, finish)
-    (finish..start).to_a.reverse.map{ |bottles| sing(bottles) }.join("\n")
-  end
-
-  def song
-    verses(99, 0)
-  end
-
-  private
-
-  def sing(bottles)
+  def verse(bottles)
     @nr_bottles = bottles
 
     "#{how_many_bottles.capitalize} of beer on the wall, "\
@@ -24,6 +10,16 @@ class Bottles
     "\n"
   end
 
+  def verses(start, finish)
+    (finish..start).to_a.reverse.map{ |bottles| verse(bottles) }.join("\n")
+  end
+
+  def song
+    verses(99, 0)
+  end
+
+  private
+
   def what_to_do
     if @nr_bottles > 0
       message = "Take #{one_or_it} down and pass it around"
@@ -32,16 +28,11 @@ class Bottles
       message = "Go to the store and buy some more"
       @nr_bottles = 99
     end
-
     message
   end
 
   def how_many_bottles
-    message = if @nr_bottles > 0
-      nr_bottles
-    else
-      "no more bottles"
-    end
+    @nr_bottles > 0 ? nr_bottles : "no more bottles"
   end
 
   def one_or_it
